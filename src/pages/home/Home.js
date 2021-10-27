2e8ee92b175ed83e211f4c8111d17cdee1a339d0
bipinrai054 <bipinrai054@gmail.com>
2021-10-27 17:30:25 +0545

    featured item component done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,12 @@
+import FeaturedInfo from '../../component/featuredInfo/FeaturedInfo';
 import './Home.css';
 
 function Home() {
-  return <div className='home'>home</div>;
+  return (
+    <div className='home'>
+      <FeaturedInfo />
+    </div>
+  );
 }
 
 export default Home;
